6b129417945b6ebc53d6b9519357aacba325fd56
jpiskorik <jjp74@pitt.edu>
2016-11-07 17:06:55 -0500

    further merge conflicts

--- src/test/java/chess/PawnTest.java
+++ src/test/java/chess/PawnTest.java
@@ -7,51 +7,6 @@ import org.mockito.Mockito;
 
 public class PawnTest {
 
-<<<<<<< HEAD
-	ChessBoard board = Mockito.mock(ChessBoard.class);
-	Pawn p = new Pawn("pawn", true, true, true, 1, 0);
-	
-	//test if normal move for a pawn is correct, the pawn has not been moved and move one square horizontally
-	//Top of board
-	//int array contains 0, 2 is expected
-	@Test
-	public void testFirstMoveOneSquareVerticallyTop() {
-		Mockito.when(board.getPieceAt(2, 0)).thenReturn(null);
-		int[] expected = {2, 0};
-		assertArrayEquals(expected, p.move(board, 2, 0));
-	}
-
-	//test if normal move for a pawn is correct, the pawn has not been moved and move one square horizontally
-	//Bottom of board
-	//int array contains 0, 2 is expected
-	@Test
-	public void testFirstMoveOneSquareVerticallyBottom() {
-		Pawn pawn = new Pawn("pawn", true, true, false, 6, 0);
-		Mockito.when(board.getPieceAt(5, 0)).thenReturn(null);
-		int[] expected = {5, 0};
-		assertArrayEquals(expected, pawn.move(board, 5, 0));
-	}
-	
-	//test if normal move for a pawn is correct, the pawn has not been moved and move two square horizontally
-	//int array 0, 3
-	//Top of board
-	@Test
-	public void testFirstMoveTwoSquaresVerticallyTop() {
-		Mockito.when(board.getPieceAt(3, 0)).thenReturn(null);
-		int[] expected = {3, 0};
-		assertArrayEquals(expected, p.move(board, 3, 0));
-	}
-
-	//test if normal move for a pawn is correct, the pawn has not been moved and move two square horizontally
-	//int array 0, 3
-	//Bottom of board
-	@Test
-	public void testFirstMoveTwoSquaresVerticallyBottom() {
-		Pawn pawn = new Pawn("pawn", true, true, false, 6, 0);
-		Mockito.when(board.getPieceAt(4, 0)).thenReturn(null);
-		int[] expected = {4, 0};
-		assertArrayEquals(expected, pawn.move(board, 4, 0));
-=======
 	Pawn p = new Pawn("pawn", true, true, false, 1, 0);
 	//mocked objects for test purpose
 	private ChessBoard board = Mockito.mock(ChessBoard.class);
@@ -73,7 +28,6 @@ public class PawnTest {
 		Mockito.when(board.getPieceAt(3, 0)).thenReturn(null);
 		int[] expected = {3, 0};
 		assertArrayEquals(expected, p.move(board, 3, 0));
->>>>>>> master
 	}
 	
 	//test the illegal backwards move is not taken
@@ -100,9 +54,6 @@ public class PawnTest {
 	public void testMoveHorizontally() {
 		Mockito.when(board.getPieceAt(1, 1)).thenReturn(null);
 		int[] expected = {1, 0};
-<<<<<<< HEAD
-		assertArrayEquals(expected, p.move(board, 1, 1));	
-=======
 		assertArrayEquals(expected, p.move(board, 1, 1));
 	}
 	
@@ -189,7 +140,6 @@ public class PawnTest {
 		Mockito.when(board.getPieceAt(8, 0)).thenReturn(null);
 		int[] expected = {7, 0};
 		assertArrayEquals(expected, pawn.move(board, 8, 0));
->>>>>>> master
 	}
 
 	//test if not first move and attempts to move two
